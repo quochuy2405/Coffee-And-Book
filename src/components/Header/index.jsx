b1f4ba027d78b5a3ee6a40e2fe0e02b829adef9a
Bùi Quốc Huy <65577247+1771009414huy@users.noreply.github.com>
2021-10-01 17:36:12 +0700

    ok

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,18 @@
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
 import React, { useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./styles.scss";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: "0 4px",
+  },
+}));
 const List_NavLink = [
   {
     tilte_link: "Đặt Hàng",
@@ -16,66 +28,76 @@ const List_NavLink = [
   },
   {
     tilte_link: "Khuyến mãi",
-  }
+  },
 ];
 
 function Header(props) {
-
-
   function ChangeActive(class_Name) {
     const Loaiactive = document.querySelector(".chon.active");
-    const changeadrss=document.querySelector(".Hearder__IpAddress")
+    const changeadrss = document.querySelector(".Hearder__IpAddress");
     if (Loaiactive) {
       Loaiactive.classList.remove("active");
     }
     document.querySelector(class_Name).classList.add("active");
-    
-    if(class_Name===".choose_mangdi")
-    {
-      document.querySelector(".choose_input_text").placeholder="Tìm cửa hàng theo quận huyện";
-      changeadrss.querySelector(".Trans__img img").src="https://minio.thecoffeehouse.com/images/tch-web-order/Pickup2.png";
-      changeadrss.querySelector(".Trans__text p:first-child").innerText ="Mang đi";
-      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText ="Tại:Tìm cửa hàng theo quận huyện";
-    }
-    else{
-      document.querySelector(".choose_input_text").placeholder="Vui lòng nhập địa chỉ";
-      changeadrss.querySelector(".Trans__img img").src="https://minio.thecoffeehouse.com/images/tch-web-order/Delivery2.png";
-      changeadrss.querySelector(".Trans__text p:first-child").innerText ="Giao hàng";
-      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText ="Tại:Vui lòng nhập địa chỉ";
-    }
 
+    if (class_Name === ".choose_mangdi") {
+      document.querySelector(".choose_input_text").placeholder = "Tìm cửa hàng theo quận huyện";
+      changeadrss.querySelector(".Trans__img img").src =
+        "https://minio.thecoffeehouse.com/images/tch-web-order/Pickup2.png";
+      changeadrss.querySelector(".Trans__text p:first-child").innerText = "Mang đi";
+      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText = "Tại:Tìm cửa hàng theo quận huyện";
+    } else {
+      document.querySelector(".choose_input_text").placeholder = "Vui lòng nhập địa chỉ";
+      changeadrss.querySelector(".Trans__img img").src =
+        "https://minio.thecoffeehouse.com/images/tch-web-order/Delivery2.png";
+      changeadrss.querySelector(".Trans__text p:first-child").innerText = "Giao hàng";
+      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText = "Tại:Vui lòng nhập địa chỉ";
+    }
   }
   useEffect(() => {
-    const changeadrss=document.querySelector(".Hearder__IpAddress")
-    document.querySelector(".choose_input_text").addEventListener('input', function (evt) {
-      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText=`Tại:${this.value}`;
+    const changeadrss = document.querySelector(".Hearder__IpAddress");
+    document.querySelector(".choose_input_text").addEventListener("input", function (evt) {
+      changeadrss.querySelector(".Trans__text p:nth-child(2)").innerText = `Tại:${this.value}`;
+    });
   });
-  })
 
   return (
     <div className="Header">
       <label htmlFor="check_choose"></label>
-      <input id="check_choose" type="checkbox" style={{display:"none"}} name=""/>
+      <input id="check_choose" type="checkbox" style={{ display: "none" }} name="" />
       <label className="overlay" htmlFor="check_choose"></label>
-      <div  className="choose_ForBuy">
+      <div className="choose_ForBuy">
         <div className="choose">
-              <div className="chon choose_giaohang active"onClick={()=>ChangeActive(".choose_giaohang")} >
-                 <img className="choose_img" src="https://minio.thecoffeehouse.com/images/tch-web-order/Delivery2.png" alt="" />
-                  <b className="choose_text" >Giao hàng</b>
-              </div>
-              <div className="chon choose_mangdi" onClick={()=>ChangeActive(".choose_mangdi")}>
-                     <img className="choose_img" src="https://minio.thecoffeehouse.com/images/tch-web-order/Pickup2.png" alt="" />
-                     <b className="choose_text">Mang đi</b>
-              </div>
+          <div className="chon choose_giaohang active" onClick={() => ChangeActive(".choose_giaohang")}>
+            <img
+              className="choose_img"
+              src="https://minio.thecoffeehouse.com/images/tch-web-order/Delivery2.png"
+              alt=""
+            />
+            <b className="choose_text">Giao hàng</b>
+          </div>
+          <div className="chon choose_mangdi" onClick={() => ChangeActive(".choose_mangdi")}>
+            <img
+              className="choose_img"
+              src="https://minio.thecoffeehouse.com/images/tch-web-order/Pickup2.png"
+              alt=""
+            />
+            <b className="choose_text">Mang đi</b>
+          </div>
         </div>
-        <div className="choose_input">  
-          <input className="choose_input_text" type="text" name="" id="" placeholder="Vui lòng nhập địa chỉ"/>
-          <img className="btn_clear" data-v-367d85a4="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGYSURBVHgBrZTbccIwEEVXmoFv//MYuwNSQUIFSSoI6SCpAKggoQLoIKQCJhXEHZjw+Oebl3OvxwqKLRtC2BmwtCsf35V2pcRhURR5lUqlo5S6juO4BZefhkL4prvd7r3ZbI5c76qsYzab3Wmthxh6UmIpuJ8Fa3uyWCxeAHs7BqNBuc8PL5fLrlMhA1jUk/PstV6vP/8AoayDx1D+Yfv9/h7pjw0wksPG27aSfPouX+LfbDaBTtX52SjS72PBFYZT4+NB0MeYA5hUhsaiW9fXttvtKAgCAtqEErZer9v0MZYqzYq4UVD4iXErG7QBqEufPjOGkokrK75DYCwFZkM5L4MZ03JhI3AqR9RRGX/2nhbwQh5K6AisbBjTrFarkwzUdShfGn8fDqAHwIO9Z2w1A2VMHLWINWOV3iyRFBSrnFjY3KJarRZopLBC2zyK27wTfcKbh8/klNmDkDuQM42dY66xX/fhfD7vQXr3j7BBo9F4MvPcBZv2NqG+lFuyVczOdqqi1QSzz3m6cmhN9nTIykDpsNdzpfMNzwr4YQY4/g8AAAAASUVORK5CYII=" alt=""></img>
+        <div className="choose_input">
+          <input className="choose_input_text" type="text" name="" id="" placeholder="Vui lòng nhập địa chỉ" />
+          <img
+            className="btn_clear"
+            data-v-367d85a4=""
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGYSURBVHgBrZTbccIwEEVXmoFv//MYuwNSQUIFSSoI6SCpAKggoQLoIKQCJhXEHZjw+Oebl3OvxwqKLRtC2BmwtCsf35V2pcRhURR5lUqlo5S6juO4BZefhkL4prvd7r3ZbI5c76qsYzab3Wmthxh6UmIpuJ8Fa3uyWCxeAHs7BqNBuc8PL5fLrlMhA1jUk/PstV6vP/8AoayDx1D+Yfv9/h7pjw0wksPG27aSfPouX+LfbDaBTtX52SjS72PBFYZT4+NB0MeYA5hUhsaiW9fXttvtKAgCAtqEErZer9v0MZYqzYq4UVD4iXErG7QBqEufPjOGkokrK75DYCwFZkM5L4MZ03JhI3AqR9RRGX/2nhbwQh5K6AisbBjTrFarkwzUdShfGn8fDqAHwIO9Z2w1A2VMHLWINWOV3iyRFBSrnFjY3KJarRZopLBC2zyK27wTfcKbh8/klNmDkDuQM42dY66xX/fhfD7vQXr3j7BBo9F4MvPcBZv2NqG+lFuyVczOdqqi1QSzz3m6cmhN9nTIykDpsNdzpfMNzwr4YQY4/g8AAAAASUVORK5CYII="
+            alt=""
+          ></img>
         </div>
       </div>
-      <div  className="Hearder__TilteName">
+      <div className="Hearder__TilteName">
         <Link to="/">
-        <h4>THE COFFEE HOUSE</h4>
+          <h4>THE COFFEE HOUSE</h4>
         </Link>
       </div>
       <label htmlFor="check_choose" id="Header_tran" className="Hearder__IpAddress">
@@ -91,9 +113,9 @@ function Header(props) {
           <i className="fas fa-chevron-down"></i>
         </div>
       </label>
-      <div id="Nav_Link"className="Header__NavLink">
+      <div id="Nav_Link" className="Header__NavLink">
         <ul>
-          {List_NavLink.map((Item,index) =>
+          {List_NavLink.map((Item, index) =>
             Item.link ? (
               <li key={index}>
                 <NavLink to={Item.link}>
@@ -102,7 +124,7 @@ function Header(props) {
               </li>
             ) : (
               <li key={index}>
-                <p >{Item.tilte_link}</p>
+                <p>{Item.tilte_link}</p>
               </li>
             )
           )}
@@ -118,9 +140,13 @@ function Header(props) {
 
         <div className="icon_store">
           {" "}
+              <StyledBadge badgeContent={4} color="secondary">
           <Link to="/Checkout">
-          <i class="fas fa-shopping-bag"></i>
-          </Link>
+            
+              <i class="fas fa-shopping-cart"></i></Link>
+              </StyledBadge>
+       
+          
         </div>
       </div>
     </div>
